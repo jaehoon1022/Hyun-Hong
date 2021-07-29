c3155e94447377a472e8804bb883d1349f16134e
gustmd213 <51289147+gustmd213@users.noreply.github.com>
2021-07-29 17:55:24 +0900

    기본 틀

---
+++ js/app.js
@@ -0,0 +1,9 @@
+ 
+  const loginBtn = document.querySelector("#login-form input:last-child");
+  
+  function loginBtnClick() {
+    location.href = "statistics.html";
+  }
+  
+  loginBtn.addEventListener("click", loginBtnClick);
+  
